797895c704c43261bd2b39f61a2ea5a82b3b368e
Mohamed BN abd alhamed <99559957+MHMD-BN@users.noreply.github.com>
2025-01-27 20:56:21 +0200

    Add files via upload

---
+++ Quran/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDQeWpg4bkUMyAir76OLl4-4d9cFExx37Q",
+    authDomain: "quran-11747.firebaseapp.com",
+    projectId: "quran-11747",
+    storageBucket: "quran-11747.firebasestorage.app",
+    messagingSenderId: "484969787827",
+    appId: "1:484969787827:web:fd2b841c1a513a0816dbc6",
+    measurementId: "G-ZNC30E63VL"
+};
+
+export { firebaseConfig };
